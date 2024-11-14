3009c169544b81fd002755012b54129576b7b74f
Christos Gkovaris <christosgkovariscs52@gmail.com>
2024-11-14 11:14:19 +0200

    Add files via upload

---
+++ Operations/string_equal.py
@@ -0,0 +1,13 @@
+# Are two strings equal?
+
+def check_strings(string1, string2):
+    # The function returns TRUE if the strings are equal
+    return string1 == string2
+
+# Give the two input strings for checking
+string1 = input("Give the first string: ")
+string2 = input("Give the second string: ")
+
+# Call the function and start the action
+results = check_strings(string1, string2)
+print(results)
